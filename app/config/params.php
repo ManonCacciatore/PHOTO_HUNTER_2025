f95122fbb26e4f680ab671a475df6673053eb26f
“Lunoronti” <cacciatoremanon8@gmail.com>
2025-04-17 15:39:08 +0200

    Structure MVC de base

---
+++ app/config/params.php
@@ -0,0 +1,10 @@
+<?php
+
+// Initiation des zones dynamiques
+$content = "Hop";
+
+// PARAMETRES DE CONNEXION
+define('DB_NAME', "photo_hunter");
+define('DB_HOST', "localhost:3307");
+define('DB_USER', "root");
+define('DB_PASSWORD', "root");
